f74a13b65e613045ceffdaece17bf27d73449f0c
Reed Hillmar <reedhillmar@gmail.com>
2023-07-13 09:52:51 -0600

    Add test code for iteration 3 methods

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -35,4 +35,53 @@ describe Team do
       expect(team.player_count).to eq(2)
     end
   end
+
+  describe "#long_term_players & #short_term_players" do
+    it "can categorize players by contract length" do
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+
+      expect(team.long_term_players).to eq([player_1, player_3])
+      expect(team.short_term_players).to eq([player_2, player_4])
+    end
+  end
+
+  describe "#total_value" do
+    it "can give the total cost of the team" do
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+
+      expect(team.total_value).to eq(85200000)
+    end
+  end
+
+  describe "#details" do
+    it "can give team details that include total value and player count" do
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+
+      expect(team.details).to eq({"total_value" => 85200000, "player_count" => 4})
+    end
+  end
 end
