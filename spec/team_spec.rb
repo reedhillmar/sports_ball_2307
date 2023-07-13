ef659e0e7468b5d7e38915c27cb75e6539b11a90
Reed Hillmar <reedhillmar@gmail.com>
2023-07-13 10:07:48 -0600

    Add test code to test iteration 4 methods

--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -84,4 +84,36 @@ describe Team do
       expect(team.details).to eq({"total_value" => 85200000, "player_count" => 4})
     end
   end
+
+  describe "#average_cost_of_player" do
+    it "can give the average player cost in dollar format" do
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+
+      expect(team.average_cost_of_player).to eq("$21,300,000")
+    end
+  end
+
+  describe "#players_by_last_name" do
+    it "can categorize players by their last name" do
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+
+      expect(team.players_by_last_name).to eq("DeNunez, McClennan, Palledorous, Porter")
+    end
+  end
 end
